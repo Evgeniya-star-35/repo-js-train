455e4697b8a3af4e8eb9b5614bba83d70fc052bc
Evgeniya Bayda <baje75128@gmail.com>
2021-05-29 08:43:09 +0300

    ++

--- js/vova.js
+++ js/vova.js
@@ -1,1 +1,13 @@
-/*определение типа переменной пишем console.log(typeOf(название переменной))*/
+let result = 0.1 + 0.2;
+console.log(result);
+// console.log(Math.floor) // кругляет в меньшую сторону и дает только целую часть(0)
+// console.log(Math.ceil) //округляет к большему (1)
+// console.log(Math.random)
+// console.log(result.toFixed(1)); //получаем 0.3 лучше использовать с дробніми числами!!!
+
+
+// let name = 'Bob';
+// let sliceName = name.slice(0, 1); //первой указіваем букву,с которой начинаем работать, а второй -индекс,на кот-м мі заканчиваем,и он не попадает в финальній рез-т
+// console.log(name);
+// console.log(sliceName)
+
