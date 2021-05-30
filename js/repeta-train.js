39b1b334cbdc78d2a1fcd1ac9b5edfd0e73e84ef
Evgeniya Bayda <baje75128@gmail.com>
2021-05-30 18:00:49 +0300

    122

---
+++ js/repeta-train.js
@@ -0,0 +1,25 @@
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxx  calculateTolalPrice  xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+/*Напиши функцию calculateTotalPrice(items) которая принимает массив цен(чисел) и возр,их сумму*/
+
+// const calculateTotalPrice = function (items) {
+//     let total = 0;
+//     for (const item of items) {
+//         total += item;
+        
+//     }
+//     return total;
+// }
+// console.log(calculateTotalPrice([1, 2, 3]));
+// console.log(calculateTotalPrice([5, 10, 15, 20]));
+// console.log(calculateTotalPrice([100, 200, 300]));
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxx напиши ф-ию logItems (items) для пребора и логирования массива   xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// const logItems = function (items) {
+//     for (const item of items) {
+//         console.log(item);
+//     }
+// }
+// logItems(['Mango', 'Kivi']); єта ф-ия ничего не возвращает просто получает массив и логирует в консоль
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxx напиши ф-ию logItems (items) для пребора и логирования массива   xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
