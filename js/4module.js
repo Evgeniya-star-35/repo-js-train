2fc22171f1542bca4856889463ce8434b995c0b7
Evgeniya Bayda <baje75128@gmail.com>
2021-06-07 19:47:17 +0300

    абстрагирование повторения

---
+++ js/4module.js
@@ -0,0 +1,70 @@
+//  Абстрагирование повторения
+
+
+// const printMessage = function (message) {
+//   console.log(message);
+// };
+
+// const higherOrderFunction = function (callback) {
+//   const string = 'HOCs are awesome';
+//   callback(string);
+// };
+
+// higherOrderFunction(printMessage);
+
+
+// for (let i = 0; i < 10; i += 1) {
+//   console.log(i);
+// }
+// const repeatLog = function (n) {
+//   for (let i = 0; i < n; i += 1) {
+//     console.log(i);
+//   }
+// };
+
+// repeatLog(5);
+// const printValue = function (value) {
+//   console.log(value);
+// };
+
+// const prettyPrint = function (value) {
+//   console.log('Logging value: ', value);
+// };
+
+// const repeat = function (n, action) {
+//   for (let i = 0; i < n; i += 1) {
+//     action(i);
+//   }
+// };
+
+// // Передаем printValue как callback-функцию
+// repeat(3, printValue);
+// // 0
+// // 1
+// // 2
+
+// Передаем prettyPrint как callback-функцию
+// repeat(3, prettyPrint);
+// Logging value: 0
+// Logging value: 1
+// Logging value: 2
+// const repeat = function (n, action) {
+//   for (let i = 0; i < n; i += 1) {
+//     action(i);
+//   }
+// };
+
+// const labels = [];
+
+// repeat(5, value => {
+//   labels.push(`Label ${value + 1}`);
+// });
+
+// console.log(labels); // ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"]
+
+
+
+// Фильтрация массива
+
+
+
