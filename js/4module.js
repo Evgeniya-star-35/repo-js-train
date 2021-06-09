7a3135fa9499bc9003e2017af52456a7200ad585
Evgeniya Bayda <baje75128@gmail.com>
2021-06-09 21:45:33 +0300

    филтр

--- js/4module.js
+++ js/4module.js
@@ -129,8 +129,36 @@
 // console.log("в коде после тайм-аута");  
 
 
+// *Операция фильтрации ххх
+
+const filter = function (array, test) {
+    const filteredArray = [];
+for (const el of array) {
+    console.log(el);
+    const passed = test(el);
+    if (passed) {
+        filteredArray.push(el);
+    }
+    }
+    return filteredArray;
+};
+// 1.надо передать ф-ю
+// 2.фун-я получает элемент массива
+// 3.если элемент массива удовлетворяет условию,то ф-я вернет true
+// 4.если элемент массива  НЕ удовлетворяет условию,то ф-я вернет false
 
 
+// const callback1 = function (value) {
+//     return value >= 3;
+// }
+// const r1 = filter([1, 2, 3, 4, 5], callback1);
+// console.log(r1);
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], function (value) {
+//     return value <= 4;
+// });
+// console.log(r2); 
+
+//              фрукты  
 
 
 
