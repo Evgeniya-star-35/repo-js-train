7a02b52dac3605d608dd7ad7d387296c5b16d494
Evgeniya Bayda <baje75128@gmail.com>
2021-06-25 14:47:25 +0300

    task

--- js/zadachi.js
+++ js/zadachi.js
@@ -563,3 +563,23 @@
 // );
 
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// You are given an array(which will have a length of at least 3, but could be very large) containing integers.The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.Write a method that takes the array
+// as an argument and returns this "outlier" N.
+
+function findOutlier(integers) {
+  const oddArray = [];
+  const evenArray = [];
+  for (let integer of integers) {
+    if (integer % 2 === 0) {
+      evenArray.push(integer);
+    } else {
+      oddArray.push(integer);
+    }
+  }
+  return oddArray.length === 1 ? oddArray[0] : evenArray[0];
+}
+console.log(findOutlier([0, 1, 2])); //, 1)
+console.log(findOutlier([1, 2, 3])); // 2)
+console.log(findOutlier([2, 6, 8, 10, 3])); //3)
+console.log(findOutlier([0, 0, 3, 0, 0])); // 3,
+console.log(findOutlier([1, 1, 0, 1, 1])); //0);
