ddc2347eea65436cc694e1c9a435edf1dd5ece41
Evgeniya Bayda <baje75128@gmail.com>
2021-06-27 06:04:37 +0300

    +

--- js/zadachi.js
+++ js/zadachi.js
@@ -719,3 +719,24 @@
 // console.log(sortByLength(['Beg', 'Life', 'I', 'To'])); //["I", "To", "Beg", "Life"]
 // console.log(sortByLength(['', 'Moderately', 'Brains', 'Pizza'])); // ["", "Pizza", "Brains", "Moderately"]
 // console.log(sortByLength(['Longer', 'Longest', 'Short'])); // ["Short", "Longer", "Longest"]
+
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// //* Your task is to write function findSum.
+// // Upto and including n, this function will return the sum of all multiples of 3 and 5.
+// // For example:
+// // findSum(5) should return 8 (3 + 5)
+// // findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+// function findSum(n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       total += i;
+//     }
+//   }
+//   return total;
+// }
+// console.log(findSum(5)); //8
+// console.log(findSum(10)); //33
