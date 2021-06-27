a80d5e261937eade56cd53a32e1f26041a94e0be
Evgeniya Bayda <baje75128@gmail.com>
2021-06-27 06:03:47 +0300

    task

--- js/zadachi.js
+++ js/zadachi.js
@@ -664,14 +664,58 @@
 
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-function getDivisorsCnt(n) {
-  let cnt = 0;
-  for (let i = 1; i <= n; i++) {
-    if (!(n % i)) cnt += 1;
-  }
-  return cnt;
-}
-console.log(getDivisorsCnt(4)); //
-console.log(getDivisorsCnt(5));
-console.log(getDivisorsCnt(12));
-console.log(getDivisorsCnt(30));
+// function getDivisorsCnt(n) {
+//   let cnt = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (!(n % i)) cnt += 1;
+//   }
+//   return cnt;
+// }
+// console.log(getDivisorsCnt(4)); //
+// console.log(getDivisorsCnt(5));
+// console.log(getDivisorsCnt(12));
+// console.log(getDivisorsCnt(30));
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// // Given an integer as input, can you round it to the
+// // next (meaning, "higher") multiple of 5?
+
+// // Учитывая целое число в качестве входных данных,
+// //  можете ли вы округлить его до следующего
+// //   (то есть «большего») кратного 5?
+
+// function roundToNext5(n) {
+//   return Math.ceil(n / 5) * 5;
+// }
+
+// console.log(roundToNext5(2)); //5
+// console.log(roundToNext5(3)); //5
+// console.log(roundToNext5(-2)); //0
+// console.log(roundToNext5(-5)); //-5
+// console.log(roundToNext5(30)); //30
+// console.log(roundToNext5(21)); //25
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+// function sortByLength(array) {
+//   // Return an array containing the same strings, ordered from shortest to longest
+
+//   return array.sort((a, b) => a.length - b.length);
+// }
+
+// console.log(sortByLength(['Beg', 'Life', 'I', 'To'])); //["I", "To", "Beg", "Life"]
+// console.log(sortByLength(['', 'Moderately', 'Brains', 'Pizza'])); // ["", "Pizza", "Brains", "Moderately"]
+// console.log(sortByLength(['Longer', 'Longest', 'Short'])); // ["Short", "Longer", "Longest"]
