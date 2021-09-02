5d85f0fcdbfa79d3233d41d1ee647e89bc16e186
Evgeniya Bayda <baje75128@gmail.com>
2021-09-02 20:53:48 +0300

    +

---
+++ js/okryjnost.js
@@ -0,0 +1,12 @@
+const PI = 3.14159265359;
+// const circleLength = radius => {
+//   return 2 * Math.PI * radius;
+// };
+// or
+const circleLength = radius => {
+  return PI * radius ** 2;
+};
+console.log(circleLength(5));
+console.log(circleLength(12));
+// задача:
+// Функция circleLength должна возвращать длину круга с учетом радиуса<--- Вот єтот вопрос надо решить
