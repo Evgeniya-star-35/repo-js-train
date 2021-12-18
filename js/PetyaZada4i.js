8edb1532c5caa79c4e2a77dd89a29a1521a8ff07
Evgeniya Bayda <baje75128@gmail.com>
2021-12-18 13:31:23 +0200

    Update PetyaZada4i.js

--- js/PetyaZada4i.js
+++ js/PetyaZada4i.js
@@ -39,3 +39,103 @@ console.log(
     '111:100',
   ]),
 ); // => 17);
+function getAge(age) {
+  const startDate = 1970;
+
+  const curDate = Date.now() - age.getTime();
+  const ageDate = new Date(curDate);
+  const result = Math.abs(ageDate.getUTCFullYear() - startDate);
+  return result;
+}
+
+
+function getWeekDay(date) {
+  return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
+}
+
+
+function getAmountDaysToNewYear() {
+  let theСalendrMonth = 12;
+  let compMonth = 11;
+  let lastDayDec = 31;
+  let oneThousend = 1000;
+  let sixty = 60;
+  let houerInADay = 24;
+
+  let today = new Date();
+  let NewYear = today.getFullYear();
+
+  if (today.getMonth() === theСalendrMonth && today.getDate() > lastDayDec) {
+    NewYear = NewYear + 1;
+  }
+
+  let NewYearDate = new Date(NewYear, compMonth, lastDayDec);
+  let dayMilliseconds = oneThousend * sixty * sixty * houerInADay;
+
+  return Math.ceil((NewYearDate.getTime() - today.getTime()) / dayMilliseconds);
+}
+
+
+function getProgrammersDay(year) {
+  const PROGRAMMERS_DAY = 256;
+  let programmersDateObj = new Date(year, 0, PROGRAMMERS_DAY);
+
+  let programmersDate = programmersDateObj.getDate();
+  let programmersMonth = programmersDateObj.toLocaleString('en-EN', {
+    month: 'short'
+  });
+  let programmersYear = programmersDateObj.getFullYear();
+  let programmersWeekDay = getWeekDay(programmersDate);
+
+  return `${programmersDate} ${programmersMonth}, ${programmersYear} (${programmersWeekDay})`;
+}
+
+
+function howFarIs(dataStr) {
+  const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentWeekday = new Date().toLocaleString('en', { weekday: 'long' });
+  const currentIndx = new Date().getUTCDay();
+
+  const givenWeekday = dataStr;
+  const givenIndx = weekday.map(el => el.toLowerCase()).indexOf(givenWeekday.toLowerCase());
+
+  const weekDays = 7;
+
+  if (currentIndx === givenIndx) {
+    return `Hey, today is ${ currentWeekday } =)`
+  }
+
+  if (currentIndx > givenIndx) {
+    let result = weekDays - currentIndx + givenIndx;
+    return `It's ${result} day(s) left till ${weekday[givenIndx]}.`
+  }
+  if (currentIndx < givenIndx) {
+    let result = givenIndx - currentIndx;
+    return `It's ${result} day(s) left till ${weekday[givenIndx]}.`
+  }
+}
+
+
+function isValidIdentifier(dataValid) {
+	const myReg = /^[a-zA-Z_$][a-zA-Z_$0-9]*$/
+	const result = myReg.test(dataValid)
+	return result
+}
+
+
+function capitalize(dataStrCap) {
+  return dataStrCap.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+    letter.toUpperCase()
+  );
+}
+
+
+function isValidAudioFile(dataStrAudio) {
+  dataStrAudio = /^[a-zA-Z]+(.mp3|.flac|.alac|.aac)$/.test(dataStrAudio);
+  return dataStrAudio;
+}
+
+
+function getHexadecimalColors(dataHexStr) {
+    return dataHexStr.match(/#([a-f0-9]{3}){1,2}\b/gi);
+}
